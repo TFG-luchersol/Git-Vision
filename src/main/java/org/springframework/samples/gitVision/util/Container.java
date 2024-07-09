175ea50cd893dd5ae8dfbe6f11270aef686d6c7f
luchersol <lmherencia2003@gmail.com>
2024-07-09 20:17:28 +0200

    [DOCS]: Añadir documentación de clases contenedores y retocar UML

--- src/main/java/org/springframework/samples/gitVision/util/Container.java
+++ src/main/java/org/springframework/samples/gitVision/util/Container.java
@@ -3,40 +3,46 @@ package org.springframework.samples.gitvision.util;
 import java.lang.reflect.Constructor;
 import java.lang.reflect.ParameterizedType;
 import java.lang.reflect.Type;
+import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
+import java.util.Objects;
 import java.util.stream.Collectors;
 
 /**
  * <p>A <strong>Container</strong> is a typed abstract class used for parsing queries 
- * that return Collection<Object[]></p>
+ * that return Collection< Object[]></p>
  * 
  * @param <T> Type of the elements in the container
  * 
- * @see CollectionItem
+ * @see ContainerItem
  */
 public abstract class Container<T extends ContainerItem> {
 
     protected List<T> container;
 
     public Container(Collection<Object[]> collection){
-        try {
-            Class<?> clazz = getTypeOfT();
-            Constructor<?> method = clazz.getConstructor(Object[].class);
-            List<T> container = collection.stream().map(elem -> {
-                try {
-                    @SuppressWarnings("unchecked")
-                    T mappedElem = (T) method.newInstance(elem);
-                    return mappedElem;
-                } catch (Exception e) {
-                    throw new RuntimeException(e);
-                }
-            }).collect(Collectors.toList());
-            this.container = container;
-        } catch (Exception e) {
-            throw new RuntimeException(e);
+        Objects.requireNonNull(collection, "The constructor of a Container can't receive null as a parameter");
+        if(collection.isEmpty())
+            this.container = new ArrayList<>();
+        else {
+            try {
+                Class<?> clazz = getTypeOfT();
+                Constructor<?> method = clazz.getConstructor(Object[].class);
+                List<T> container = collection.stream().map(elem -> {
+                    try {
+                        @SuppressWarnings("unchecked")
+                        T mappedElem = (T) method.newInstance(elem);
+                        return mappedElem;
+                    } catch (Exception e) {
+                        throw new RuntimeException(e);
+                    }
+                }).collect(Collectors.toList());
+                this.container = container;
+            } catch (Exception e) {
+                throw new RuntimeException(e);
+            }
         }
-        
     }
 
     public List<T> getContainer(){
--- src/main/java/org/springframework/samples/gitVision/util/ContainerItem.java
+++ src/main/java/org/springframework/samples/gitVision/util/ContainerItem.java
@@ -1,10 +1,21 @@
 package org.springframework.samples.gitvision.util;
 
+import java.util.Objects;
 
+/**
+ * <p>A <strong>ContainerItem</strong> is a abstract class used to 
+ * represent the differents elements of a <strong>Container</strong></p>
+ * 
+ * <p>The class that extends ContainerItem must have a constructor with 
+ * an Object[] parameter where the parsing of the query elements is performed</p>
+ * @param <T> Type of the elements in the container
+ * 
+ * @see Container
+ */
 public abstract class ContainerItem {
 
     public ContainerItem(Object[] objects){
-        
+        Objects.requireNonNull(objects, "The constructor of a ContainerItem can't receive null as a parameter");
     }
     
 }
