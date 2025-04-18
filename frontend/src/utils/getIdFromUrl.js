5b8730adbceeb5262742a11501111b90a4d56d4e
luchersol <lmherencia2003@gmail.com>
2025-04-18 20:25:03 +0200

    [CHORE]: Eliminar archivos no utilizados

--- frontend/src/utils/getIdFromUrl.js
+++
@@ -1,7 +0,0 @@
-export default function getIdFromUrl(index=-1) {
-    const splits = window.location.pathname.split('/')
-    if(index < 0)
-        index = splits.length + index
-    
-    return splits[index];
-}
